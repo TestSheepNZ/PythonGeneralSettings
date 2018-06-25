e28c4fc74fbdcf7e1932ee4abbf685267b0c2681
TestSheepNZ <mike_talks@hotmail.com>
2018-06-25 21:06:33 +1200

    Pushing files so far

---
+++ PlayPen.py
@@ -0,0 +1,16 @@
+""" Nothing to see here - this is just a play area for me to try out
+    Python concepts in """
+
+
+def setstring():
+    string = "hey there"
+    return string
+
+
+string = "hello world"
+
+string = setstring()
+print(string)
+
+
+
